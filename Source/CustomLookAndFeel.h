6a44173cb46df09bdadf44e5a5f0447bace34c0c
Butch Warns <mail@butchwarns.com>
2022-07-19 12:14:21 +0200

    Make drawRotarySlider method private

--- Source/CustomLookAndFeel.h
+++ Source/CustomLookAndFeel.h
@@ -16,9 +16,9 @@ class CustomLookAndFeel : public juce::LookAndFeel_V4
 public:
     CustomLookAndFeel();
     virtual ~CustomLookAndFeel();
+
+private:
     void drawRotarySlider(juce::Graphics &g, int x, int y, int width, int height,
                           float sliderPosProportional, float rotaryStartAngle,
                           float rotaryEndAngle, juce::Slider &slider) override;
-
-private:
 };
