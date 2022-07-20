cb1df167ccb61e2ecdcfeb395f4b2f79ace7374f
Butch Warns <mail@butchwarns.com>
2022-07-20 13:56:15 +0200

    Move remaining sliders into subcomponent, ..

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -10,31 +10,23 @@
 #include "PluginEditor.h"
 
 //==============================================================================
-LR_DelayAudioProcessorEditor::LR_DelayAudioProcessorEditor(LR_DelayAudioProcessor &p, juce::AudioProcessorValueTreeState &apvts)
-    : AudioProcessorEditor(&p), audioProcessor(p), apvts(apvts), utilityRotaryGroup(apvts), rotaryGroupLR(apvts)
+LR_DelayAudioProcessorEditor::LR_DelayAudioProcessorEditor(LR_DelayAudioProcessor &p)
+    : AudioProcessorEditor(&p), audioProcessor(p), rotaryGroupLR(p.getApvts())
 {
     // Set main window size
-    setSize(width, height);
+    setSize(400, 900);
 
     //==============================================================================
-    // Add children
-    addChildComponent(header);
-    addChildComponent(footer);
+    // Add as children and show all components
+    addAndMakeVisible(&header);
+    addAndMakeVisible(&footer);
 
-    addChildComponent(utilityRotaryGroup);
-    addChildComponent(rotaryGroupLR);
+    // addAndMakeVisible(&utilityRotaryGroup);
+    addAndMakeVisible(&rotaryGroupLR);
 
     //==============================================================================
     // Set "global" look and feel
     setLookAndFeel(&customLook);
-
-    //==============================================================================
-    // Show all components
-    addAndMakeVisible(&header);
-    addAndMakeVisible(&footer);
-
-    addAndMakeVisible(&utilityRotaryGroup);
-    addAndMakeVisible(&rotaryGroupLR);
 }
 
 LR_DelayAudioProcessorEditor::~LR_DelayAudioProcessorEditor()
@@ -55,47 +47,15 @@ void LR_DelayAudioProcessorEditor::resized()
 
     Rect headerArea = localBounds.removeFromTop(HEADER_HEIGHT);
     Rect footerArea = localBounds.removeFromBottom(FOOTER_HEIGHT);
-    Rect utilArea = localBounds.removeFromLeft(100);
+
+    // Rect utilArea = localBounds.removeFromLeft(100);
     Rect &rotaryLRArea = localBounds;
 
     //==============================================================================
-    // Header & footer
-
+    // Set bounds
     header.setBounds(headerArea);
     footer.setBounds(footerArea);
 
-    utilityRotaryGroup.setBounds(utilArea);
+    // utilityRotaryGroup.setBounds(utilArea);
     rotaryGroupLR.setBounds(rotaryLRArea);
-
-    //==============================================================================
-    // Slider and their labels
-    // dryWet_L_Label.setBounds(1 * rotaryWidth, HEADER_HEIGHT + 0 * roLaHeight, rotaryWidth, labelHeight);
-    // dryWet_R_Label.setBounds(2 * rotaryWidth, HEADER_HEIGHT + 0 * roLaHeight, rotaryWidth, labelHeight);
-
-    // dryWet_L_Slider.setBounds(1 * rotaryWidth, HEADER_HEIGHT + labelHeight + 0 * roLaHeight, rotaryWidth, rotaryHeight);
-    // dryWet_R_Slider.setBounds(2 * rotaryWidth, HEADER_HEIGHT + labelHeight + 0 * roLaHeight, rotaryWidth, rotaryHeight);
-
-    // feedback_L_Label.setBounds(1 * rotaryWidth, HEADER_HEIGHT + 1 * roLaHeight, rotaryWidth, labelHeight);
-    // feedback_R_Label.setBounds(2 * rotaryWidth, HEADER_HEIGHT + 1 * roLaHeight, rotaryWidth, labelHeight);
-
-    // feedback_L_Slider.setBounds(1 * rotaryWidth, HEADER_HEIGHT + labelHeight + 1 * roLaHeight, rotaryWidth, rotaryHeight);
-    // feedback_R_Slider.setBounds(2 * rotaryWidth, HEADER_HEIGHT + labelHeight + 1 * roLaHeight, rotaryWidth, rotaryHeight);
-
-    // delayTime_L_Label.setBounds(1 * rotaryWidth, HEADER_HEIGHT + 2 * roLaHeight, rotaryWidth, labelHeight);
-    // delayTime_R_Label.setBounds(2 * rotaryWidth, HEADER_HEIGHT + 2 * roLaHeight, rotaryWidth, labelHeight);
-
-    // delayTime_L_Slider.setBounds(1 * rotaryWidth, HEADER_HEIGHT + labelHeight + 2 * roLaHeight, rotaryWidth, rotaryHeight);
-    // delayTime_R_Slider.setBounds(2 * rotaryWidth, HEADER_HEIGHT + labelHeight + 2 * roLaHeight, rotaryWidth, rotaryHeight);
-
-    // cutoffLP_L_Label.setBounds(1 * rotaryWidth, HEADER_HEIGHT + 3 * roLaHeight, rotaryWidth, labelHeight);
-    // cutoffLP_R_Label.setBounds(2 * rotaryWidth, HEADER_HEIGHT + 3 * roLaHeight, rotaryWidth, labelHeight);
-
-    // cutoffLP_L_Slider.setBounds(1 * rotaryWidth, HEADER_HEIGHT + labelHeight + 3 * roLaHeight, rotaryWidth, rotaryHeight);
-    // cutoffLP_R_Slider.setBounds(2 * rotaryWidth, HEADER_HEIGHT + labelHeight + 3 * roLaHeight, rotaryWidth, rotaryHeight);
-
-    // cutoffHP_L_Label.setBounds(1 * rotaryWidth, HEADER_HEIGHT + 4 * roLaHeight, rotaryWidth, labelHeight);
-    // cutoffHP_R_Label.setBounds(2 * rotaryWidth, HEADER_HEIGHT + 4 * roLaHeight, rotaryWidth, labelHeight);
-
-    // cutoffHP_L_Slider.setBounds(1 * rotaryWidth, HEADER_HEIGHT + labelHeight + 4 * roLaHeight, rotaryWidth, rotaryHeight);
-    // cutoffHP_R_Slider.setBounds(2 * rotaryWidth, HEADER_HEIGHT + labelHeight + 4 * roLaHeight, rotaryWidth, rotaryHeight);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -22,7 +22,7 @@
 class LR_DelayAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
-    LR_DelayAudioProcessorEditor(LR_DelayAudioProcessor &p, juce::AudioProcessorValueTreeState &apvts);
+    LR_DelayAudioProcessorEditor(LR_DelayAudioProcessor &p);
     ~LR_DelayAudioProcessorEditor() override;
 
     //==============================================================================
@@ -40,31 +40,15 @@ private:
     Header header;
     Footer footer;
 
-    UtilityRotaryGroup utilityRotaryGroup;
+    // UtilityRotaryGroup utilityRotaryGroup;
     LR_RotaryGroup rotaryGroupLR;
 
-    //==============================================================================f
-    // Reference to the AudioProcessorValueTreeState object in the Processor
-    juce::AudioProcessorValueTreeState &apvts;
 
     //==============================================================================
     // Magic numbers for UI layout
-    const int rotaryWidth = 80;
-    const int textBoxHeight = 25;
-    const int rotaryHeight = 80 + textBoxHeight;
 
     const int HEADER_HEIGHT = 50;
     const int FOOTER_HEIGHT = 50;
-
-    const int labelHeight = 25;
-
-    const int roLaHeight = rotaryHeight + labelHeight;
-
-    const int rotaryCols = 3;
-    const int rotaryRows = 5;
-
-    const int height = HEADER_HEIGHT + FOOTER_HEIGHT + (rotaryRows * roLaHeight);
-    const int width = 400;
 
     //==============================================================================
     // UI styling
